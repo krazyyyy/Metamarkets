bdfffd6cc6acb003b9823aee95503af3ff0e5c6d
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2023-07-28 12:02:57 +0500

    Add files via upload

---
+++ metamarket-interview/metamarket-interview/bookmark/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+
+from accounts.models import UserProfile
+
+
+class BookmarkList(models.Model):
+    user = models.ManyToManyField(UserProfile, related_name='own_list')
+
+    list_name = models.CharField(_("리스트 이름"), max_length=50)
+    description = models.CharField(_("설명"), max_length=255)
+
+    ACCESS_LEVEL_CHOICES = (
+        ('P', _('개인')),
+        ('S', _('공개')),
+    )
+    access_level = models.CharField(
+        _("접근 권한"),
+        max_length=1,
+        choices=ACCESS_LEVEL_CHOICES,
+        default='P',
+    )
+
+    created_at = models.DateTimeField(_("생성 시간"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("수정 시간"), auto_now=True)
+
+    def __str__(self):
+        return '<' + self.list_name + '> ' + self.description
+
+
+class BookmarkItem(models.Model):
+    belonged_list = models.ForeignKey(BookmarkList, on_delete=models.CASCADE)
+
+    site_name = models.CharField(_("사이트 이름"), max_length=50)
+    site_url = models.URLField("URL")
+
+    created_at = models.DateTimeField(_("생성 시간"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("수정 시간"), auto_now=True)
+
+    def __str__(self):
+        return self.site_name
+
+    def get_absolute_url(self):
+        return reverse_lazy('bookmark:detail', args=[self.id])
+        # TODO: CBV에서 처리할 수 있는 방법은 없는가 - get_success_url 오버라이딩
+
+    def quicksort_by_site_name(cls, items):
+        if len(items) <= 1:
+            return items
+        pivot = items[len(items) // 2]
+        left = [item for item in items if item.site_name < pivot.site_name]
+        middle = [item for item in items if item.site_name == pivot.site_name]
+        right = [item for item in items if item.site_name > pivot.site_name]
+        return cls.quicksort_by_site_name(left) + middle + cls.quicksort_by_site_name(right)
